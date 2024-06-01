c12327935103637403318e82a35ed494043a500d
hayyantahirr <hayyantahir7@gmail.com>
2024-06-01 16:25:06 +0500

    rendering-object-app-initializd

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+
+function App() {
+const [name , setName]=useState("Hayyan Tahir")
+console.log(name);
+
+
+
+
+  return(
+    <><h1>hello world</h1>
+    <h1>hello</h1>
+    </>
+  )
+}
+
+export default App
